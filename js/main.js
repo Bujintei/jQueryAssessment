668840b238ae9ca718fbd294e2b5ec08a5d770fa
Brandon Nguyen <eagleeye2555@gmail.com>
2015-04-20 12:18:54 -0700

    Quiz

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var sup = document.getElementsByClassName( "dan" );
+ 
+dan.addEventListener( "click", function( event ) {
+    alert( "Hey you clicked on me again." );
+}, false );
+
+var bruh = document.getElementById( "iwao" );
+ 
+iwao.addEventListener( "click", function( event ) {
+    alert( "Hey you clicked on me again." );
+}, false );
